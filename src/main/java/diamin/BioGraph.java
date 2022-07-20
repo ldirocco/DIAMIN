3b8012dc902157c34080a992e342af70f07131ef
ldirocco <83399603+ldirocco@users.noreply.github.com>
2022-07-20 17:02:43 +0200

    Update BioGraph.java

--- src/main/java/diamin/BioGraph.java
+++ src/main/java/diamin/BioGraph.java
@@ -15,6 +15,9 @@ import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.List;
+/***
+ * Models in a Java application the concept of a distributed MIN.
+***/
 
 public class BioGraph {
     private JavaSparkContext jsc;
@@ -26,22 +29,46 @@ public class BioGraph {
         jsc.sc().setCheckpointDir("Diamin-CheckpointDir");
     }
 
+    /**
+     * Returns the number of interactors (i.e., the vertices) of the underlying graph.
+     * @return |V| - number of interactors (vertices).
+     */
     public long interactorsCounter(){return MIN.vertices().count();}
+    /**
+     * Returns the number of interactions (i.e., the edges) of the underlying graph.
+     * @return |E| - number of interactors (edges).
+     */
     public long interactionsCounter(){return MIN.edges().count();}
+
+    /**
+     * Writes on a file the unique id or the name of all the interactors (i.e., the nodes) of the underlying graph.
+     * @param filename Name of the output file.
+     */
     public void interactors(String filename) {
         //return just one file
         MIN.vertices().coalesce(1).write().format("csv").option("header", false).mode("overwrite").option("sep", ",").save(filename);
     }
+
+    /**
+     * Writes on a file the unique id or the name of all interactions (i.e., edges) of the underlying graph.
+     * @param filename Name of the output file.
+     */
     public void interactions(String filename){
         //return just one file
         MIN.edges().coalesce(1).write().format("csv").option("header",false).mode("overwrite").option("sep",",").save(filename);
     }
 
-    public Dataset<Row> interactions(){
-        return MIN.edges();
-    }
 
 
+    //public Dataset<Row> interactions(){
+    //    return MIN.edges();
+    //}
+
+
+    /**
+     * Computes the density index of the underlying graph.
+     * @return d - density index.
+     */
     public float density(){
         float nVertices=(float)MIN.vertices().count();
         float nEdges=(float)MIN.edges().count();
@@ -49,22 +76,35 @@ public class BioGraph {
         return density;
     }
 
-    public Dataset<Row> degrees(){
+    /**
+     * Returns the list of interactors of the underlying graph, with their associated degree.
+     *
+     * @param n Number of the top-scored interactors to return.
+     *
+     * @return The list of tuples <interactor_id, degree>.
+     */
+    public ArrayList<Tuple2<String, Integer>> degrees(int n){
         return MIN.degrees();
     }
-    public Row[] degrees(int n){
-        return MIN.degrees().sort("degree").take(n);
-    }
-    public Dataset<Row> inDegrees(){
-        return MIN.inDegrees();
-    }
-    public Dataset<Row> outDegrees(){
-        return MIN.outDegrees();
-    }
+    //public Row[] degrees(int n){
+    //    return MIN.degrees().sort("degree").take(n);
+    //}
+    //public Dataset<Row> inDegrees(){
+    //    return MIN.inDegrees();
+    //}
+    //public Dataset<Row> outDegrees(){
+    //    return MIN.outDegrees();
+    //}
 
 
-    public List<String> xNeighbors(String p, int x){
-        JavaRDD<Row> xNeighbors_rdd=graphMiner.xNeighbors(MIN.toGraphX(),p,x).toJavaRDD();
+    /**
+     * Returns the x-neighbors of a subset  S of interactors
+     * @param S Set of interactors.
+     * @param x Length of the path.
+     * @return The list of x-neighbors.
+     */
+    public List<String> xNeighbors(String S, int x){
+        JavaRDD<Row> xNeighbors_rdd=graphMiner.xNeighbors(MIN.toGraphX(),S,x).toJavaRDD();
 
         StructType neighbors_attributes_structure = new StructType()
                 .add(new StructField("id", DataTypes.StringType, true, Metadata.empty()))
@@ -75,9 +115,15 @@ public class BioGraph {
         return edgesDF.toJavaRDD().map(t->t.getString(0)).collect();
     }
 
-    public BioGraph xSubGraph(String interactors_string, int x) {
+    /**
+     * Returns the subgraph containing a subset S of interactors and their x-neighbors.
+     * @param S Set of interactors.
+     * @param x Length of the path.
+     * @return The BioGraph storing the subgraph of x-neighbors.
+     */
+    public BioGraph xSubGraph(String S, int x) {
         ArrayList<String> interactors = new ArrayList<>();
-        for (String interactor : interactors_string.split(",")) {
+        for (String interactor : S.split(",")) {
             interactors.add(interactor);
         }
         List<String> xNeighbors_list = graphMiner.xSubGraph(MIN.toGraphX(), interactors, x).toJavaRDD().collect();
@@ -104,8 +150,14 @@ public class BioGraph {
     }
 
 
-    public List<String> xWeightedNeighbors(String p, int x){
-        JavaRDD<Row> xNeighbors_rdd=graphMiner.xWeightedNeighbors(MIN.toGraphX(),p,x).toJavaRDD();
+    /**
+     * Returns the x-weighted-neighborhood of an input interactor.
+     * @param i Interactor Name.
+     * @param x Minimum threshold.
+     * @return The list of x-weighted-neighborhood of i.
+     */
+    public List<String> xWeightedNeighbors(String i, int x){
+        JavaRDD<Row> xNeighbors_rdd=graphMiner.xWeightedNeighbors(MIN.toGraphX(),i,x).toJavaRDD();
 
         StructType neighbors_attributes_structure = new StructType()
                 .add(new StructField("id", DataTypes.StringType, true, Metadata.empty()))
@@ -116,9 +168,16 @@ public class BioGraph {
         return edgesDF.toJavaRDD().map(t->t.getString(0)).collect();
     }
 
-    public BioGraph xWeightedSubgraph(String interactors_string, int x) {
+    /***
+     * Returns the subgraph containing an input interactor and its x-weighted-neighbors.
+     * @param i Interactor Name.
+     * @param x Minimum threshold.
+     * @return The BioGraph storing the subgraph of x-weighted neighbors.
+     */
+
+    public BioGraph xWeightedSubgraph(String i, int x) {
         ArrayList<String> interactors = new ArrayList<>();
-        for (String interactor : interactors_string.split(",")) {
+        for (String interactor : i.split(",")) {
             interactors.add(interactor);
         }
         List<String> xNeighbors_list = graphMiner.xWeightedSubgraph(MIN.toGraphX(), interactors, x).toJavaRDD().collect();
@@ -144,10 +203,15 @@ public class BioGraph {
         return xsubgraph;
     }
 
-    public Dataset<Row> closestComponents(String input_interactors){
-        ArrayList<Object> S=new ArrayList<>();
-        for(String i:input_interactors.split(",")){
-            S.add(i);
+    /**
+     * Returns the connected component of the underlying graph, containing the largest number of elements in common with an input subset of interactors.
+     * @param S su
+     * @return
+     */
+    public Dataset<Row> closestComponents(String S){
+        ArrayList<Object> interactors=new ArrayList<>();
+        for(String i:S.split(",")){
+            interactors.add(i);
         }
 
         Dataset<Row> components = MIN.connectedComponents().run();
@@ -155,7 +219,7 @@ public class BioGraph {
         Tuple2<Long, Integer> max = components.javaRDD()
                 .mapToPair(r -> new Tuple2<>(Long.parseLong(r.getString(1)), r.get(0)))
                 .mapToPair(t->{
-                    if(S.contains(t._2)) {
+                    if(interactors.contains(t._2)) {
                         return new Tuple2<Long,Integer>(t._1,1);
                     } else {
                         return new Tuple2<Long,Integer>(t._1,0);
@@ -171,11 +235,5 @@ public class BioGraph {
 
         return components.filter("component=" + max._1).select("id");
     }
-
-    public void intersectionByComponent(){
-
-    }
-
-
-
+    
 }
